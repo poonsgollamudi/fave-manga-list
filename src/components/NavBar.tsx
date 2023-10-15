23c3d88dea1dbc67f3be1d1ac1ef1c4e4f9fb0fe
poonsgollamudi <pgollamudi@gmail.com>
2023-10-15 16:38:53 -0400

    dark mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,16 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../images/LadyReadingSilhouette2.svg";
+import logo from "../images/woman-reading-book-color.jpg";
 
 const NavBar = () => {
   return (
     <HStack>
-      <Image src={logo} boxSize="50px" border="1px" borderColor="blue.500" />
+      <Image
+        src={logo}
+        boxSize="50px"
+        border="1px"
+        borderColor="blue.500"
+        borderRadius="50%"
+      />
       <Text>NavBar</Text>
     </HStack>
   );
