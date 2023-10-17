7ac16a14e517d50390e804f8f82aad4e2a4d43d3
poonsgollamudi <pgollamudi@gmail.com>
2023-10-17 15:50:20 -0400

    uuid part 2

--- src/components/MangaCard.tsx
+++ src/components/MangaCard.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Manga } from "../hooks/useMangas";
 import { Card, CardBody, Heading, Image, Link, Text } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
 
 interface Props {
   manga: Manga;
@@ -12,7 +13,7 @@ const selectedManga: Manga = { title: "Kaguya-Hime", genre: "romance" };
 const MangaCard = ({ manga, genreSelect }: Props) => {
   return (
     <>
-      <Card borderRadius={10} key={manga.mal_id}>
+      <Card borderRadius={10} key={uuidv4()}>
         <Image src={manga?.images.webp.image_url} />
         <CardBody>
           <Link href={manga?.url} fontSize="2xl" isExternal>
